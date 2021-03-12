cf982ade2f53b0a41c69a55bd3cdede28e2e5592
In-Hee Lee <ikarus97@gmail.com>
2021-03-12 01:05:39 -0500

    update build script

--- docs/buildDoc.sh
+++ docs/buildDoc.sh
@@ -30,23 +30,12 @@ for current_version in ${versions}; do
 		continue
 	fi
 
-	languages="en `find docs/locales/ -mindepth 1 -maxdepth 1 -type d -exec basename '{}' \;`"
-	for current_language in ${languages}; do
-		export current_language
+	echo "INFO: building docs"
 
-		echo "INFO: building for ${current_language}"
-	
-		# HTML
-		sphinx-build -b html docs/ docs/_build/html/${current_language}/${current_version} -D language="${current_language}"
+	#HTML
+	sphinx-build -b html docs/ docs/_build/html/${current_version} -D language="en"
 
-		# PDF 
-		sphinx-build -b rinoh docs/ docs/_build/rinoh -D language="${current_language}"
-		mkdir -p "${docroot}/${current_language}/${current_version}"
-		cp "docs/_build/rinoh/target.pdf" "${docroot}/${current_language}/${current_version}/helloWorld-docs_${current_language}_${current_version}.pdf"
-
-		# copy static assets into docroot
-		rsync -av "docs/_build/html/" "${docroot}/"
-	done
+	rsync -av "docs/_build/html/" "${docroot}/"
 done
 
 git checkout master
